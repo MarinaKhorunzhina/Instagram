cd50316cc5d8227fe12c0e904a2762e605adad1c
MarinaKhorunzhina <101120283+MarinaKhorunzhina@users.noreply.github.com>
2023-01-23 20:46:58 +0300

    MakeLeftBarButtonItem

--- Instagram/Feed/FeedViewController.swift
+++ Instagram/Feed/FeedViewController.swift
@@ -12,6 +12,26 @@ class FeedViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        initialize()
+    }
+}
+private extension FeedViewController {
+    func initialize() {
+        view.backgroundColor = .white
+        navigationItem.leftBarButtonItems = makeLeftBarButtonItem()
+    }
+    
+    func makeLeftBarButtonItem() -> [UIBarButtonItem] {
+        let dropDownButtonItem = UIBarButtonItem (title: nil, image: UIImage(systemName: "chevron.down"), primaryAction: nil, menu: makeDropDownMenu())
+       return [dropDownButtonItem]
+    }
+    func makeDropDownMenu() -> UIMenu {
+        let subsItem = UIAction(title: "Подписки", image: UIImage(systemName: "person.2")) {_ in
+            print ("Подписки")
+        }
+        let favsItem = UIAction(title: "Избранное", image: UIImage(systemName: "star")) {_ in
+            print ("Избранные")
+        }
+        return UIMenu(title: "", children: [subsItem, favsItem])
     }
 }
