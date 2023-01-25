126550d1e848b74f86b495d96a12a980bfede6f8
MarinaKhorunzhina <101120283+MarinaKhorunzhina@users.noreply.github.com>
2023-01-25 20:21:56 +0300

    MakeLeftBarButtonItem, makeRightBarButtonItem

--- Instagram/Feed/FeedViewController.swift
+++ Instagram/Feed/FeedViewController.swift
@@ -18,7 +18,9 @@ class FeedViewController: UIViewController {
 private extension FeedViewController {
     func initialize() {
         view.backgroundColor = .white
+        navigationController?.navigationBar.tintColor = .black
         navigationItem.leftBarButtonItems = makeLeftBarButtonItem()
+        navigationItem.rightBarButtonItems = makeRightBarButtonItem()
     }
     
     func makeLeftBarButtonItem() -> [UIBarButtonItem] {
@@ -26,6 +28,19 @@ private extension FeedViewController {
         let dropDownButtonItem = UIBarButtonItem (title: nil, image: UIImage(systemName: "chevron.down"), primaryAction: nil, menu: makeDropDownMenu())
        return [logoBarButtonItem, dropDownButtonItem]
     }
+    func makeRightBarButtonItem () -> [UIBarButtonItem] {
+        let addBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "plus.app"), style: .plain, target: self, action: #selector(didTapPlusButton))
+        let directBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "paperplane"), style: .plain, target: self, action: #selector(didTapDirectButton))
+        return [addBarButtonItem, directBarButtonItem]
+    }
+    
+    @objc func didTapPlusButton() {
+        
+    }
+    @objc func didTapDirectButton() {
+        
+    }
+    
     func makeDropDownMenu() -> UIMenu {
         let subsItem = UIAction(title: "Подписки", image: UIImage(systemName: "person.2")) {_ in
             print ("Подписки")
